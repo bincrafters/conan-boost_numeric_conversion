12f7f031a54be04c5cb3d37d194df87ec140fbce
Rene Rivera <grafikrobot@gmail.com>
2017-12-20 21:14:33 -0600

    Initial 1.66.0 package branche.

--- conanfile.py
+++ conanfile.py
@@ -3,16 +3,16 @@ from conans import ConanFile
 
 class BoostNumeric_ConversionConan(ConanFile):
     name = "Boost.Numeric_Conversion"
-    version = "1.65.1"
+    version = "1.66.0"
 
     requires = \
-        "Boost.Config/1.65.1@bincrafters/testing", \
-        "Boost.Conversion/1.65.1@bincrafters/testing", \
-        "Boost.Core/1.65.1@bincrafters/testing", \
-        "Boost.Mpl/1.65.1@bincrafters/testing", \
-        "Boost.Preprocessor/1.65.1@bincrafters/testing", \
-        "Boost.Throw_Exception/1.65.1@bincrafters/testing", \
-        "Boost.Type_Traits/1.65.1@bincrafters/testing"
+        "Boost.Config/1.66.0@bincrafters/testing", \
+        "Boost.Conversion/1.66.0@bincrafters/testing", \
+        "Boost.Core/1.66.0@bincrafters/testing", \
+        "Boost.Mpl/1.66.0@bincrafters/testing", \
+        "Boost.Preprocessor/1.66.0@bincrafters/testing", \
+        "Boost.Throw_Exception/1.66.0@bincrafters/testing", \
+        "Boost.Type_Traits/1.66.0@bincrafters/testing"
 
     lib_short_names = ["numeric_conversion"]
     is_header_only = True
@@ -20,9 +20,9 @@ class BoostNumeric_ConversionConan(ConanFile):
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-numeric_conversion"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "www.boost.org/users/license.html"
-    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    build_requires = "Boost.Generator/1.66.0@bincrafters/testing"
     short_paths = True
     exports = "boostgenerator.py"
 
