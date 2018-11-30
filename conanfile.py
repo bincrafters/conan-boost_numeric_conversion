fa7f0987592c492607343087f7838d76ecb76aee
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 22:01:12 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostNumeric_ConversionConan(base.BoostBaseConan):
     name = "boost_numeric_conversion"
@@ -20,5 +20,3 @@ class BoostNumeric_ConversionConan(base.BoostBaseConan):
         "boost_throw_exception",
         "boost_type_traits"
     ]
-
-
