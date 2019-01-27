7748e2826caee0801cb6dce4707fa9cd17aed905
Jerry W <jerrywiltse@gmail.com>
2019-01-26 23:58:45 -0500

    re-implement version numbers, fix travis badges, refresh ci scripts, move to python 3.6 on ci

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostNumeric_ConversionConan(base.BoostBaseConan):
     name = "boost_numeric_conversion"
+    version = "1.68.0"
     url = "https://github.com/bincrafters/conan-boost_numeric_conversion"
     lib_short_names = ["numeric_conversion"]
     header_only_libs = ["numeric_conversion"]
