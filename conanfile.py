0b42364c03a858e0e14f1bac23f23ea27d53cc51
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:06 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,20 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostNumeric_ConversionConan(base.BoostBaseConan):
     name = "boost_numeric_conversion"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_numeric_conversion"
-    lib_short_names = ["numeric_conversion"]
-    header_only_libs = ["numeric_conversion"]
-    b2_requires = [
-        "boost_config",
-        "boost_conversion",
-        "boost_core",
-        "boost_mpl",
-        "boost_preprocessor",
-        "boost_throw_exception",
-        "boost_type_traits"
-    ]
+    version = "1.70.0"
